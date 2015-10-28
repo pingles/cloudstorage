21459f6b4b50432f711c9aff39de2aee7db1f209
Paul Ingles <paul@oobaloo.co.uk>
2015-10-28 14:10:06 +0000

    delete unused files

--- src/cloudstorage/core.clj
+++
@@ -1,6 +0,0 @@
-(ns cloudstorage.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- test/cloudstorage/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns cloudstorage.core-test
-  (:require [clojure.test :refer :all]
-            [cloudstorage.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
